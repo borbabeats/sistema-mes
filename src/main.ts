6b4932d8009e7729a1ac675e179db04ca781925f
borbabeats <maiconbsconceicao>
2026-05-06 16:26:59 -0300

    fix: setting cors

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,9 @@ async function bootstrap() {
 
   // Configuração do CORS
   const allowedOrigins = [
-    '107.20.14.182:3001',
-    'https://d399mtdh0ga8g7.cloudfront.net'
+    'http://d399mtdh0ga8g7.cloudfront.net',
+    'https://d399mtdh0ga8g7.cloudfront.net',
+    'http://frontend-mes-195950944161-us-east-1-an.s3-website-us-east-1.amazonaws.com'
   ];
   
   app.enableCors({
